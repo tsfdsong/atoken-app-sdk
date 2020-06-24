7408a9cc9f7affdfa8160c8725af391e7dea4cb5
汤松 <simon.tang@dfgroup.pro>
2020-06-24 17:41:18 +0800

    update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@ rm -rf output/*
 mkdir -p output/ios/
 mkdir -p output/android/
 echo "Building for iOS..."
-CGO_CFLAGS_ALLOW='-fmodules|-fblocks' gomobile bind -target=ios -o=output/ios/atoken.framework github.com/tsfdsong/atoken-app-sdk/blockchain github.com/tsfdsong/atoken-app-sdk/vexchain github.com/tsfdsong/neo-utils
+
+gomobile bind -target=ios -o=output/ios/atoken.framework github.com/tsfdsong/atoken-app-sdk/blockchain github.com/tsfdsong/atoken-app-sdk/vexchain github.com/tsfdsong/neo-utils
 
 echo "Building for Android..."
-ANDROID_HOME=/Users/$USER/Library/Android/sdk/android-ndk-r21 gomobile bind -target=android -o=output/android/atoken.aar github.com/tsfdsong/atoken-app-sdk/blockchain github.com/tsfdsong/atoken-app-sdk/vexchain github.com/tsfdsong/neo-utils
+gomobile bind -target=android -o=output/android/atoken.aar github.com/tsfdsong/atoken-app-sdk/blockchain github.com/tsfdsong/atoken-app-sdk/vexchain github.com/tsfdsong/neo-utils
+
+echo "Building for zip..."
+zip -r "atoken-$(date +"%Y%m%d-%H:%M:%S").zip" output/ios/atoken.framework output/android/*
