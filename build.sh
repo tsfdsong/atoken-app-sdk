35047068afa9221ceed05a3b8d5f9a2cec4fa0f1
汤松 <simon.tang@dfgroup.pro>
2020-07-13 11:40:50 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ mkdir -p output/ios/
 mkdir -p output/android/
 echo "Building for iOS..."
 
-gomobile bind -target=ios -o=output/ios/atoken.framework github.com/tsfdsong/atoken-app-sdk/blockchain github.com/tsfdsong/atoken-app-sdk/vexchain github.com/tsfdsong/neo-utils
+gomobile bind -target=ios -o=output/ios/atoken.framework github.com/tsfdsong/atoken-app-sdk/blockchain github.com/tsfdsong/atoken-app-sdk/defi github.com/tsfdsong/atoken-app-sdk/vexchain github.com/tsfdsong/neo-utils
 
 echo "Building for Android..."
-gomobile bind -target=android -o=output/android/atoken.aar github.com/tsfdsong/atoken-app-sdk/blockchain github.com/tsfdsong/atoken-app-sdk/vexchain github.com/tsfdsong/neo-utils
+gomobile bind -target=android -o=output/android/atoken.aar github.com/tsfdsong/atoken-app-sdk/blockchain github.com/tsfdsong/atoken-app-sdk/defi github.com/tsfdsong/atoken-app-sdk/vexchain github.com/tsfdsong/neo-utils
 
 echo "Building for zip..."
 mkdir -p build/
